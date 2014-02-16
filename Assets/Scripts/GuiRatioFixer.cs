dab6bf151f203da03462b07ffa4b279ee98c08a5
Orillion <hegemansmichiel@gmail.com>
2014-02-16 18:46:38 +0100

    Splash Screen implemented

---
+++ Assets/Scripts/GuiRatioFixer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Use this on a guiText or guiTexture object to automatically have
+// it adjust its aspect ratio when the game starts.
+
+public class GuiRatioFixer : MonoBehaviour {
+
+    public float m_NativeRatio = 1.333333333333F;
+
+	// Use this for initialization
+	void Start () {
+	   float currentRatio = (float)Screen.width / (float)Screen.height;
+       Vector3 scale = transform.localScale;
+       scale.x *= m_NativeRatio / currentRatio;
+       transform.localScale = scale;
+	}
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+    public int m_BoxWidth = 100;
+    public int m_BoxHeight = 90;
+
+    public int m_ButtonWidth = 80;
+    public int m_ButtonHeight = 20;
+
+    void OnGUI() {
+        GUI.Box(new Rect((Screen.width / 2) - (m_BoxWidth / 2), (Screen.height / 2) - (m_BoxHeight / 2), m_BoxWidth, m_BoxHeight), "Main Menu");
+        if (GUI.Button(new Rect((Screen.width / 2) - (m_ButtonWidth / 2), (Screen.height / 2) - (m_ButtonHeight / 2) - 5, m_ButtonWidth, m_ButtonHeight), "Start")) {
+            // Start application.
+        }
+
+        if (GUI.Button(new Rect((Screen.width / 2) - (m_ButtonWidth / 2), (Screen.height / 2) - (m_ButtonHeight / 2) + 25, m_ButtonWidth, m_ButtonHeight), "Exit")) {
+            // Exit application.
+            Application.Quit();
+        }
+    }	
+}
